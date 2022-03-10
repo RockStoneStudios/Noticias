21a5a328870c163844f5975830dcee0c43f8816a
RockStone <RockStoneStudios666@gmail.com>
2022-03-10 17:41:21 -0500

    Fin Fin

--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -3,14 +3,17 @@ import React from 'react'
 const Noticia = ({noticia}) => {
     //extraemos datos
     const {urlToImage,url,title,description,source} = noticia;
-      
+      const imagen = (urlToImage) ? (
+        <div className="card-image">
+        <img src={urlToImage} alt={title} /> 
+        <div className="card-title">{source.name}</div>
+      </div>  
+      )
+      :null
   return (
       <div className="col s12 m6 l4">
        <div className="card">
-       <div className="card-image">
-         <img src={urlToImage} alt={title} /> 
-         <div className="card-title">{source.name}</div>
-         </div>  
+      
          <div className="card-content">
           <h3>{title}</h3>
           <p>{description}</p>
